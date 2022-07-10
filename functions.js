6c9eee730236b9beb61a937c897f0ea066fd679f
Jayanto Mitra <37693777+jayantomitra@users.noreply.github.com>
2022-07-10 13:45:43 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,46 @@
+// *************** javascript functions ********************
+
+// function statement =>  aka  =>  function declaration
+
+      function a(){
+        console.log("a is called")
+      }
+
+
+//function expression => aka => function variable
+
+      var b = function (){
+        console.log("b is called")
+      }
+
+
+
+// Anonymous functions
+
+      function (){
+      // not allowed in es6
+      }
+
+
+// Named function expression
+
+var b = function xyz (){
+  console.log("b is called")
+}
+
+
+// Difference between parameters & arguments
+
+var s = function pqr (param1, param2){ //<-- para,eters
+  console.log("pqr is called")
+}
+
+s(1,2)  //<-- arguments
+
+
+
+// First class functions
+
+
+
+// Arrow functions
